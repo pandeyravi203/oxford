010ba15777755afc5d6a9a61f98e6d5e61adc37c
Ravi Pandey <pandeyravi203@gmail.com>
2018-07-27 12:09:32 +0530

    Update form.py

--- oxford/form.py
+++ oxford/form.py
@@ -1,5 +1,5 @@
 from django import forms
 from django.contrib.auth.models import User
 class Myform(forms.Form):
-    Word=forms.TextField(label='Enter the Word',max_length=300,)
+    Word=forms.CharField(label='Enter the Word',max_length=300,)
 
